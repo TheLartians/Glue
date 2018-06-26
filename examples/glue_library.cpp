92160f9a6404996f841f599f041edd9b543e78ea
Lars Melchior <TheLartians@users.noreply.github.com>
2018-06-26 20:37:43 +0200

    update

--- examples/glue_library.cpp
+++ examples/glue_library.cpp
@@ -1,18 +1,17 @@
-/* test.c */
-
-#include <iostream>
-
 #include <duktape/duktape.h>
 
 #include <lars/any.h>
 #include <lars/mutator.h>
 #include <lars/component_visitor.h>
+#include <lars/destructor.h>
 
 #include <lars/log.h>
+#include <lars/to_string.h>
 
 #include <unordered_map>
 
 #define DUK_MEMORY_DEBUG_LOG(X)
+#define DUK_DETAILED_ERRORS
 
 namespace lars{
   
@@ -50,9 +49,8 @@ namespace duk_glue{
   namespace class_helper{
   
     template <class T> duk_ret_t class_destructor(duk_context *ctx){
-      // The object to delete is passed as first argument instead
+      // The object to delete is passed as first argument
       duk_get_prop_string(ctx, 0, DUK_HIDDEN_SYMBOL("deleted"));
-      
       bool deleted = duk_to_boolean(ctx, -1);
       duk_pop(ctx);
       
@@ -97,7 +95,7 @@ namespace duk_glue{
   template <class T> void push_class(duk_context *ctx){
     auto internal_class_name = std::string( DUK_HIDDEN_SYMBOL() ) + lars::get_type_name<T>();
     auto exists = duk_get_global_string(ctx, internal_class_name.c_str());
-    if(exists) return;
+    if(exists){ return; }
     
     duk_pop(ctx);
     duk_push_c_function(ctx, &class_helper::class_constructor<T>, 0);
@@ -105,22 +103,28 @@ namespace duk_glue{
     duk_push_object(ctx);
     duk_put_prop_string(ctx, -2, "prototype");
     
-    // Now store the Point function as a global
     duk_dup_top(ctx);
     duk_put_global_string(ctx, internal_class_name.c_str() );
   }
   
-  template <class T> T & get_object(duk_context *ctx){
-    duk_get_prop_string(ctx, -1, DUK_HIDDEN_SYMBOL("data"));
+  template <class T> T * get_object_ptr(duk_context *ctx,int idx = -1){
+    auto has_data = duk_get_prop_string(ctx, idx, DUK_HIDDEN_SYMBOL("data"));
+    if(!has_data){ duk_pop(ctx); return nullptr; }
     auto ptr = duk_to_pointer(ctx, -1);
     duk_pop(ctx);
     DUK_MEMORY_DEBUG_LOG("get: " << ptr);
+    if(!ptr) return nullptr;
     auto * res = static_cast<internal_type<T> *>(ptr);
-    if(!res) throw std::runtime_error("expected pointer");
-    if(res->first != lars::get_type_index<T>()) throw std::runtime_error("type mismatch");
-    return res->second;
+    if(res->first != lars::get_type_index<T>()) return nullptr;
+    return &res->second;
   }
-  
+
+  template <class T> T & get_object(duk_context *ctx,int idx = -1){
+    auto ptr = get_object_ptr<T>(ctx,idx);
+    if(!ptr) throw std::runtime_error("cannot extract c++ object");
+    return *ptr;
+  }
+
   template <class T> T & create_and_push_object(duk_context *ctx){
     push_class<T>(ctx);
     duk_new(ctx, 0);
@@ -153,42 +157,48 @@ namespace duk_glue{
   void push_value(duk_context * ctx,lars::Any value){
     using namespace lars;
     
-    struct PushVisitor:public ConstVisitor<AnyScalarData<double>,AnyScalarData<std::string>,AnyScalarData<lars::AnyFunction>>{
+    struct PushVisitor:public ConstVisitor<AnyScalarData<double>,AnyScalarData<std::string>,AnyScalarData<lars::AnyFunction>,AnyScalarData<int>>{
       duk_context * ctx;
-      void visit(const AnyScalarData<int> &data){ duk_push_int(ctx, data.data); }
-      void visit(const AnyScalarData<double> &data){ duk_push_number(ctx, data.data); }
-      void visit(const AnyScalarData<std::string> &data){ duk_push_string(ctx, data.data.c_str()); }
-      void visit(const AnyScalarData<lars::AnyFunction> &data){ push_function(ctx,data.data); }
+      void visit(const AnyScalarData<int> &data)override{ duk_push_int(ctx, data.data); }
+      void visit(const AnyScalarData<double> &data)override{ duk_push_number(ctx, data.data); }
+      void visit(const AnyScalarData<std::string> &data)override{ duk_push_string(ctx, data.data.c_str()); }
+      void visit(const AnyScalarData<lars::AnyFunction> &data)override{ push_function(ctx,data.data); }
     } visitor;
     
     visitor.ctx = ctx;
     
-    value.accept_visitor(visitor);
+    try{
+      value.accept_visitor(visitor);
+    }catch(lars::IncompatibleVisitorException){
+      create_and_push_object<Any>(ctx) = value;
+    };
+    
   }
   
   lars::Any extract_value(duk_context * ctx,duk_idx_t idx,lars::TypeIndex type){
     auto assert_value_exists = [](auto && v){ if(!v) throw std::runtime_error("invalid argument type"); return v; };
     
+    if(auto ptr = get_object_ptr<lars::Any>(ctx,idx)) return *ptr;
+    
     if(type == lars::get_type_index<std::string>()){ return lars::make_any<std::string>(assert_value_exists(duk_to_string(ctx, idx))); }
     else if(type == lars::get_type_index<double>()){ return lars::make_any<double>(assert_value_exists(duk_to_number(ctx, idx))); }
-    else if(type == lars::get_type_index<int>()){ return lars::make_any<double>(assert_value_exists(duk_to_int(ctx, idx))); }
+    else if(type == lars::get_type_index<int>()){ return lars::make_any<int>(assert_value_exists(duk_to_int(ctx, idx))); }
     else if(type == lars::get_type_index<lars::AnyFunction>()){
       auto key = add_to_stash(ctx);
-      
-      // todo: delete key
-      // todo: add arguments (create variadic anyfunction)
-      
-      lars::AnyFunction f = [=](){
+      lars::AnyFunction f = [=,destructor = lars::make_shared_destructor([=](){ remove_from_stash(ctx, key); })](lars::AnyArguments &args){
         push_from_stash(ctx, key.c_str());
-        std::vector<lars::Any> args;
         for(auto && arg:args) push_value(ctx, arg);
         duk_call(ctx, args.size());
       };
-      
       return lars::make_any<lars::AnyFunction>(f);
-      
     }
-    else{ throw std::runtime_error("cannot convert argument"); }
+    else{
+#ifdef DUK_DETAILED_ERRORS
+      throw std::runtime_error("cannot convert argument: " + lars::stream_to_string(type.name()) + " from " + duk_to_string(ctx, idx));
+#else
+      throw std::runtime_error("cannot convert argument");
+#endif
+    }
   }
 
   void push_function(duk_context * ctx,const lars::AnyFunction &f){
@@ -199,9 +209,15 @@ namespace duk_glue{
       auto &f = get_object<lars::AnyFunction>(ctx);
       duk_pop(ctx);
       duk_pop(ctx);
-      
+       
       std::vector<lars::Any> args;
-      for(int i=0;i<f.argument_count();++i) args.emplace_back(extract_value(ctx, i, f.argument_type(i)));
+      
+      auto argc = f.argument_count();
+      if(argc == -1) argc = duk_get_top(ctx);
+      
+      LARS_LOG("call with args: " << argc);
+      for(int i=0;i<argc;++i) args.emplace_back(extract_value(ctx, i, f.argument_type(i)));
+
       auto result = f.call(args);
       
       if(result){
@@ -210,7 +226,7 @@ namespace duk_glue{
       }
       
       return 0;
-    }, f.argument_count());
+    }, f.argument_count() != -1 ? f.argument_count() : DUK_VARARGS);
     
     create_and_push_object<lars::AnyFunction>(ctx) = f;
     duk_put_prop_string(ctx, -2, DUK_HIDDEN_SYMBOL("data"));
@@ -226,7 +242,6 @@ public:
   
   duk_context * ctx = nullptr;
   
-  
   void connect_function(const std::string &name,const lars::AnyFunction &f)override{
     duk_glue::push_function(ctx,f);
     duk_put_global_string(ctx, name.c_str());
@@ -234,21 +249,22 @@ public:
   
 };
 
-void log(std::string str){
-  std::cout << "log: " << str << std::endl;
-}
-
 
 int main(int argc, char *argv[]) {
-  
   lars::Module module;
   
   module.add_function("meaning_of_life",[](){ return 42; });
   module.add_function("log",[](const std::string &str){ std::cout << "log: " << str << std::endl; });
-  module.add_function("add",[](double a,double b){ return a+b; });
+  module.add_function("add",[](double a,int b){ return a+b; });
   module.add_function("get_log_function",[](const std::string &prompt){ return lars::AnyFunction([=](std::string str){ std::cout << prompt << ": " << str << std::endl; }); });
-  module.add_function("call_callback",[](const lars::AnyFunction &f){ f(); });
+  module.add_function("call_callback",[](const lars::AnyFunction &f){ f(42); });
   
+  static unsigned my_class_instances = 0;
+  struct MyClass{ std::string data; MyClass(){ my_class_instances++; } ~MyClass(){ my_class_instances--; } };
+  module.add_function("create_my_class",[](){ return MyClass(); });
+  module.add_function("get_my_class_data",[](const MyClass &my_class){ return my_class.data; });
+  module.add_function("set_my_class_data",[](MyClass &my_class,const std::string &str){ my_class.data = str; });
+
   duk_context *ctx = duk_create_heap(NULL, NULL, NULL, NULL, NULL);
   
   DuktapeGlue glue;
@@ -256,11 +272,17 @@ int main(int argc, char *argv[]) {
   
   module.connect(glue);
 
-  duk_eval_string_noresult(ctx, "log(meaning_of_life());");
-  duk_eval_string_noresult(ctx, "log('2+3=' + add(2, 3));");
-  duk_eval_string_noresult(ctx, "log('hello world!');");
+  duk_eval_string_noresult(ctx, "var value = meaning_of_life(); if(value != 42) throw 'wrong universe!'");
+  duk_eval_string_noresult(ctx, "log('hello world!')");
+  duk_eval_string_noresult(ctx, "log('2+3=' + add(2, 3))");
   duk_eval_string_noresult(ctx, "var new_log = get_log_function('test'); new_log(add(2,40))");
-  duk_eval_string_noresult(ctx, "function myLog(){ log('myLog!'); }; call_callback(myLog);");
+  duk_eval_string_noresult(ctx, "function myLog(x){ log('myLog: ' + x); }; call_callback(myLog)");
+  duk_eval_string_noresult(ctx, "var my_class = create_my_class()");
+  duk_eval_string_noresult(ctx, "set_my_class_data(my_class,'data')");
+  duk_eval_string_noresult(ctx, "log(get_my_class_data(my_class))");
+  duk_eval_string_noresult(ctx, "set_my_class_data()");
 
+  assert(my_class_instances == 0);
+  
   return 0;
 }
