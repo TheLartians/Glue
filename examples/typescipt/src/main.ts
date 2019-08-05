d6e6bd0459871c980fdafd3a730d6a87d6d742be
Lars Melchior <TheLartians@users.noreply.github.com>
2019-08-06 00:53:02 +0200

    update

--- examples/typescipt/src/main.ts
+++ examples/typescipt/src/main.ts
@@ -1,10 +1,20 @@
 
+class Greeter {
+  name: string;
+
+  constructor(name: string){
+    this.name = name;
+  }
+
+  greet() {
+    lib.log(`Hello ${this.name}!`);
+  }
+};
+
+const greeter = new Greeter("C++");
+greeter.greet();
+
+
 const a = lib.A.create(46);
 const b = lib.B.create(-5);
 console.assert(a.add(b).next().data() == 42);
-lib.log("Hello from TypeScript!");
-
-let m = new Map<string, number>();
-m.set("test",42);
-console.log("look: ", m.get("test"));
-
