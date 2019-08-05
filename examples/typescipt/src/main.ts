594ddce8cb3ff991d91d7bd49c5e9f53d49790a3
Lars Melchior <TheLartians@users.noreply.github.com>
2019-08-06 00:47:16 +0200

    update

--- examples/typescipt/src/main.ts
+++ examples/typescipt/src/main.ts
@@ -3,3 +3,8 @@ const a = lib.A.create(46);
 const b = lib.B.create(-5);
 console.assert(a.add(b).next().data() == 42);
 lib.log("Hello from TypeScript!");
+
+let m = new Map<string, number>();
+m.set("test",42);
+console.log("look: ", m.get("test"));
+
