bfa3536871ed1226b15cbec108f3c006b504837c
Lars Melchior <TheLartians@users.noreply.github.com>
2019-08-12 19:21:38 +0200

    add erase and insert array element methods

--- include/glue/array_element.h
+++ include/glue/array_element.h
@@ -8,7 +8,7 @@ namespace glue {
   template <class T, class V = typename T::value_type> Element ArrayElement() {
     return glue::ClassElement<T>()
     .addConstructor()
-    .addMethod("push", [](T &arr, V v){ arr.emplace_back(v); })
+    .addMethod("push", [](T &arr, V v){ arr.emplace_back(std::move(v)); })
     .addMethod("size", [](const T &arr){ return arr.size(); })
     .addMethod("pop", [](T &arr){ 
       if (arr.size() == 0) throw std::runtime_error("cannot pop empty array"); 
@@ -24,7 +24,15 @@ namespace glue {
       if (idx >= arr.size()) {
         throw std::runtime_error("invalid array index");
       } 
-      arr[idx] = v;
+      arr[idx] = std::move(v);
+    })
+    .addMethod("erase", [](T &arr, size_t idx){ 
+      if (arr.size() <= idx) throw std::runtime_error("invalid array index"); 
+      arr.erase(arr.begin() + idx); 
+    })
+    .addMethod("insert", [](T &arr, size_t idx, V v){ 
+      if (arr.size() < idx) throw std::runtime_error("invalid array insert index"); 
+      arr.insert(arr.begin() + idx, v); 
     })
     .addMethod("clear", [](T &arr){ arr.clear(); })
     ;
