cc1ad543ac09988c67f8f9eec069a0f208b8f5a6
Lars Melchior <TheLartians@users.noreply.github.com>
2019-08-13 18:19:02 +0200

    array methods (#34)
    * add array clear method
    
    * add erase and insert array element methods
    
    * move when inserting

--- tests/array_element.cpp
+++ tests/array_element.cpp
@@ -13,11 +13,27 @@ TEST_CASE("array element") {
   CHECK_NOTHROW(array["push"](42));
   REQUIRE(array["size"]()->get<int>() == 1);
   CHECK(array["get"](0)->get<int>() == 42);
-  CHECK_NOTHROW(array["push"](3.141));
+  CHECK_NOTHROW(array["push"](3.141)); // implicit conversion to int
   REQUIRE(array["size"]()->get<int>() == 2);
   CHECK(array["get"](1)->get<int>() == 3);
   CHECK_NOTHROW(array["set"](0,-3));
   CHECK(array["get"](0)->get<int>() == -3);
-  CHECK_NOTHROW(array["pop"]());
-  REQUIRE(array["size"]()->get<int>() == 1);
+  CHECK_NOTHROW(array["insert"](1,5));
+  REQUIRE(array["size"]()->get<int>() == 3);
+  CHECK(array["get"](1)->get<int>() == 5);
+  CHECK_NOTHROW(array["erase"](0));
+  REQUIRE(array["size"]()->get<int>() == 2);
+  CHECK(array["get"](0)->get<int>() == 5);
+  CHECK(array["get"](1)->get<int>() == 3);
+
+  SECTION("pop"){
+    CHECK_NOTHROW(array["pop"]());
+    REQUIRE(array["size"]()->get<int>() == 1);
+  }
+
+  SECTION("clear"){
+    CHECK_NOTHROW(array["clear"]());
+    REQUIRE(array["size"]()->get<int>() == 0);
+  }
+
 }
