6d3b4ab10200f2963c60794d5c287bffbed30e13
Lars Melchior <TheLartians@users.noreply.github.com>
2019-10-02 15:02:36 +0200

    use fundamental numeric types (#40)
    * use fundamental numeric types
    
    * update tests
    
    * update tests

--- tests/lua_glue.cpp
+++ tests/lua_glue.cpp
@@ -12,56 +12,73 @@ TEST_CASE("LuaState","[lua]"){
   lua.openStandardLibs();
   
   SECTION("get nothing"){
-    REQUIRE(!lua.get("nil"));
-    REQUIRE_THROWS(lua.get<int>("nil"));
+    CHECK(!lua.get("nil"));
+    CHECK_THROWS(lua.get<int>("nil"));
   }
   
   SECTION("get number"){
-    REQUIRE_NOTHROW(lua.run("x = 40"));
-    REQUIRE(lua.get<char>("x+2") == 42);
-    REQUIRE(lua.get<int>("x+2") == 42);
-    REQUIRE(lua.get<unsigned>("x+2") == 42);
-    REQUIRE(lua.get<long>("x+2") == 42);
-    REQUIRE(lua.get<size_t>("x+2") == 42);
-    REQUIRE(lua.get<float>("x+2") == 42);
-    REQUIRE(lua.get<double>("(x/16-0.4)*2") == Approx(4.2));
+    CHECK_NOTHROW(lua.run("x = 40"));
+    CHECK(lua.get<char>("x+2") == 42);
+    CHECK(lua.get<int>("x+2") == 42);
+    CHECK(lua.get<unsigned>("x+2") == 42);
+    CHECK(lua.get<long>("x+2") == 42);
+    CHECK(lua.get<size_t>("x+2") == 42);
+    CHECK(lua.get<float>("x+2") == 42);
+    CHECK(lua.get<double>("(x/16-0.4)*2") == Approx(4.2));
+  }
+
+  SECTION("numeric conversions") {
+    lua.run("function identity(x) return x end");
+    
+    CHECK(lua["identity"](float(42.24)).get<float>() == Approx(42.24f));
+    CHECK(lua["identity"]((long double)(42.24)).get<long double>() == Approx(42.24));
+
+    CHECK(lua["identity"]((short)(42)).get<short>() == 42);
+    CHECK(lua["identity"]((int)(42)).get<int>() == 42);
+    CHECK(lua["identity"]((long)(42)).get<long>() == 42);
+    CHECK(lua["identity"]((long long)(42)).get<long long>() == 42);
+
+    CHECK(lua["identity"]((unsigned short)(42)).get<unsigned short>() == 42);
+    CHECK(lua["identity"]((unsigned int)(42)).get<unsigned int>() == 42);
+    CHECK(lua["identity"]((unsigned long)(42)).get<unsigned long>() == 42);
+    CHECK(lua["identity"]((unsigned long long)(42)).get<unsigned long long>() == 42);
   }
 
   SECTION("set number"){
-    REQUIRE(lua["tostring"](int(42)).get<std::string>() == "42");
-    REQUIRE(lua["tostring"](unsigned(42)).get<std::string>() == "42");
-    REQUIRE(lua["tostring"](size_t(42)).get<std::string>() == "42");
-    REQUIRE(lua["tostring"](char(42)).get<std::string>() == "42");
-    REQUIRE(lua["tostring"](42.0).get<std::string>() == "42.0");
+    CHECK(lua["tostring"](int(42)).get<std::string>() == "42");
+    CHECK(lua["tostring"](unsigned(42)).get<std::string>() == "42");
+    CHECK(lua["tostring"](size_t(42)).get<std::string>() == "42");
+    CHECK(lua["tostring"]('x').get<std::string>() == "x");
+    CHECK(lua["tostring"](42.0).get<std::string>() == "42.0");
   }
   
   SECTION("get boolean"){
-    REQUIRE(lua.get<bool>("true") == true);
-    REQUIRE(lua.get<bool>("not true") == false);
+    CHECK(lua.get<bool>("true") == true);
+    CHECK(lua.get<bool>("not true") == false);
   }
   
   SECTION("get string"){
-    REQUIRE(lua.get<std::string>("'Hello Lua!'") == "Hello Lua!");
+    CHECK(lua.get<std::string>("'Hello Lua!'") == "Hello Lua!");
   }
   
   SECTION("get and call function"){
-    REQUIRE(lua.get<lars::AnyFunction>("function(x) return x+1 end")(41).get<int>() == 42);
+    CHECK(lua.get<lars::AnyFunction>("function(x) return x+1 end")(41).get<int>() == 42);
   }
 
   SECTION("set value"){
     lua.set("y", 21);
-    REQUIRE(lua.get<int>("2*y") == 42);
+    CHECK(lua.get<int>("2*y") == 42);
   }
   
   SECTION("set lua value"){
     auto x = lua.get("42");
     lua.set("y",x);
-    REQUIRE(lua.get<int>("y") == 42);
+    CHECK(lua.get<int>("y") == 42);
   }
   
   SECTION("set and call function"){
     lua.set("f", lars::AnyFunction([](int x){ return x+3; }));
-    REQUIRE(lua.get<int>("f(39)") == 42);
+    CHECK(lua.get<int>("f(39)") == 42);
   }
 
   SECTION("get element map"){
@@ -70,38 +87,38 @@ TEST_CASE("LuaState","[lua]"){
     auto &map = *ptr;
     
     SECTION("get values"){
-      REQUIRE(map["a"].get<int>() == 1);
-      REQUIRE(map["b"].get<std::string>() == "b");
-      REQUIRE(map["c"]["d"].get<int>() == 42);
+      CHECK(map["a"].get<int>() == 1);
+      CHECK(map["b"].get<std::string>() == "b");
+      CHECK(map["c"]["d"].get<int>() == 42);
     }
     
     SECTION("set values"){
-      REQUIRE_NOTHROW(map["a"] = "42");
-      REQUIRE(map["a"].get<std::string>() == "42");
-      REQUIRE(lua.get<std::string>("x.a") == "42");
+      CHECK_NOTHROW(map["a"] = "42");
+      CHECK(map["a"].get<std::string>() == "42");
+      CHECK(lua.get<std::string>("x.a") == "42");
     }
     
     SECTION("update table"){
-      REQUIRE_NOTHROW(map["a"]["b"]["c"] = 42);
-      REQUIRE(map["a"]["b"]["c"].get<int>() == 42);
-      REQUIRE(lua.get<int>("x.a.b.c") == 42);
+      CHECK_NOTHROW(map["a"]["b"]["c"] = 42);
+      CHECK(map["a"]["b"]["c"].get<int>() == 42);
+      CHECK(lua.get<int>("x.a.b.c") == 42);
       map["a"]["d"] = 3;
-      REQUIRE(lua.get<int>("x.a.d") == 3);
+      CHECK(lua.get<int>("x.a.d") == 3);
     }
     
     SECTION("inspect table"){
-      REQUIRE(map.keys().size() == 3);
-      REQUIRE_NOTHROW(map["e"]["f"] = 1);
-      REQUIRE_NOTHROW(map["e"]["f"] = 1);
-      REQUIRE(map["e"].asMap());
-      REQUIRE(map["e"].asMap()->keys().size() == 2);
+      CHECK(map.keys().size() == 3);
+      CHECK_NOTHROW(map["e"]["f"] = 1);
+      CHECK_NOTHROW(map["e"]["f"] = 1);
+      CHECK(map["e"].asMap());
+      CHECK(map["e"].asMap()->keys().size() == 2);
     }
   }
   
   SECTION("global table"){
     lua.run("x = 42");
-    REQUIRE(lua["x"].get<int>() == 42);
-    REQUIRE(lua["tostring"](lua["x"]).get<std::string>() == "42.0");
+    CHECK(lua["x"].get<int>() == 42);
+    CHECK(lua["tostring"](lua["x"]).get<std::string>() == "42.0");
   }
   
   SECTION("extended element"){
@@ -111,14 +128,14 @@ TEST_CASE("LuaState","[lua]"){
     Element extends;
     setExtends(extends, base);
     lua["e"] = extends;
-    REQUIRE(lua.get<int>("e.a") == 1);
-    REQUIRE(lua.get<int>("e.b") == 2);
+    CHECK(lua.get<int>("e.a") == 1);
+    CHECK(lua.get<int>("e.b") == 2);
     extends["a"] = 3;
-    REQUIRE(lua.get<int>("e.a") == 3);
-    REQUIRE(lua.get<int>("e.b") == 2);
+    CHECK(lua.get<int>("e.a") == 3);
+    CHECK(lua.get<int>("e.b") == 2);
     extends["a"] = Any();
-    REQUIRE(lua.get<int>("e.a") == 1);
-    REQUIRE(lua.get<int>("e.b") == 2);
+    CHECK(lua.get<int>("e.a") == 1);
+    CHECK(lua.get<int>("e.b") == 2);
   }
   
   SECTION("class element"){
@@ -150,17 +167,17 @@ TEST_CASE("LuaState","[lua]"){
     
     SECTION("value and inheritance"){
       lua.run("a = A.create(42)");
-      REQUIRE_THAT(lua.get<std::string>("tostring(a)"), Catch::Matchers::Contains("A"));
-      REQUIRE(lua.get<int>("Base.value(a)") == 42);
-      REQUIRE(lua.get<int>("A.value(a)") == 42);
-      REQUIRE(lua.get<int>("a:value()") == 42);
+      CHECK_THAT(lua.get<std::string>("tostring(a)"), Catch::Matchers::Contains("A"));
+      CHECK(lua.get<int>("Base.value(a)") == 42);
+      CHECK(lua.get<int>("A.value(a)") == 42);
+      CHECK(lua.get<int>("a:value()") == 42);
     }
 
     SECTION("shared pointer value"){
       lua.run("b = A.create_shared(420)");
-      REQUIRE_THAT(lua.get<std::string>("tostring(b)"), Catch::Matchers::Contains("Base"));
-      REQUIRE(lua.get<int>("Base.value(b)") == 420);
-      REQUIRE(lua.get<int>("b:value()") == 420);
+      CHECK_THAT(lua.get<std::string>("tostring(b)"), Catch::Matchers::Contains("Base"));
+      CHECK(lua.get<int>("Base.value(b)") == 420);
+      CHECK(lua.get<int>("b:value()") == 420);
     }
 
     SECTION("default compare"){
@@ -183,13 +200,13 @@ TEST_CASE("LuaState","[lua]"){
     };
     lua["a"] = MemoryCounter(count);
     lua["b"] = MemoryCounter(count);
-    REQUIRE(count == 2);
+    CHECK(count == 2);
     lua["b"] = Any();
     lua.collectGarbage();
-    REQUIRE(count == 1);
+    CHECK(count == 1);
     lua["a"] = Any();
     lua.collectGarbage();
-    REQUIRE(count == 0);
+    CHECK(count == 0);
   }
   
   SECTION("operators"){
@@ -232,8 +249,8 @@ TEST_CASE("LuaState","[lua]"){
 
   SECTION("errors"){
     SECTION("lua errors"){
-      REQUIRE_THROWS_AS(lua.run("error('Hello Lua!')"), LuaState::Error);
-      REQUIRE_THROWS_WITH(lua.run("error('Hello Lua!')"), Catch::Matchers::Contains("Hello Lua!"));
+      CHECK_THROWS_AS(lua.run("error('Hello Lua!')"), LuaState::Error);
+      CHECK_THROWS_WITH(lua.run("error('Hello Lua!')"), Catch::Matchers::Contains("Hello Lua!"));
     }
   }
 
@@ -241,7 +258,7 @@ TEST_CASE("LuaState","[lua]"){
     lua["f"] = [](const lars::AnyArguments &args){
       return args.size();
     };
-    REQUIRE(lua.get<int>("f(1,2,3,4)") == 4);
+    CHECK(lua.get<int>("f(1,2,3,4)") == 4);
   }
 
   SECTION("callbacks"){
@@ -249,8 +266,8 @@ TEST_CASE("LuaState","[lua]"){
       lua["count"] = [](lars::AnyFunction f){
         for (int i = 0; i<10; ++i) f(i);
       };
-      REQUIRE_NOTHROW(lua.run("res = 0; count(function(i) res = res + i; end);"));
-      REQUIRE_NOTHROW(lua.get<int>("res") == 45);
+      CHECK_NOTHROW(lua.run("res = 0; count(function(i) res = res + i; end);"));
+      CHECK_NOTHROW(lua.get<int>("res") == 45);
     }
     SECTION("class values"){
       struct A{ int value; };
@@ -261,12 +278,12 @@ TEST_CASE("LuaState","[lua]"){
 
       SECTION("checks"){
         lua["a"] = A{42};
-        REQUIRE(lua.get<int>("a:value()") == 42);
+        CHECK(lua.get<int>("a:value()") == 42);
         A a{42};
         lua["a"] = std::reference_wrapper(a);
-        REQUIRE(lua.get<int>("a:value()") == 42);
+        CHECK(lua.get<int>("a:value()") == 42);
         lua["a"] = std::reference_wrapper(std::as_const(a));
-        REQUIRE(lua.get<int>("a:value()") == 42);
+        CHECK(lua.get<int>("a:value()") == 42);
       }
 
       lua["count"] = [](lars::AnyFunction f){ 
@@ -276,17 +293,17 @@ TEST_CASE("LuaState","[lua]"){
           f(std::as_const(a));
         };
       };
-      REQUIRE_NOTHROW(lua.run("res = 0; count(function(i) res = res + i:value(); end);"));
-      REQUIRE_NOTHROW(lua.get<int>("res") == 90);
+      CHECK_NOTHROW(lua.run("res = 0; count(function(i) res = res + i:value(); end);"));
+      CHECK_NOTHROW(lua.get<int>("res") == 90);
     }
     
   }
   
   SECTION("convert objects from lua object and back"){
     lua["f"] = [](const Any &v)->AnyReference{ return v; };
-    REQUIRE(lua.get<int>("f(42)") == 42);
-    REQUIRE(lua.get<int>("f({a = 42}).a") == 42);
-    REQUIRE((*lua.get<std::shared_ptr<glue::Map>>("f({a = 42})"))["a"].get<int>() == 42);
+    CHECK(lua.get<int>("f(42)") == 42);
+    CHECK(lua.get<int>("f({a = 42}).a") == 42);
+    CHECK((*lua.get<std::shared_ptr<glue::Map>>("f({a = 42})"))["a"].get<int>() == 42);
   }
 
   SECTION("run file"){
